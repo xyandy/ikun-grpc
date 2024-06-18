a1b877a5f8d50145959a44c89ea013b339025ec4
yangxu3 <yangxu3>
2024-06-18 18:49:35 +0800

    add build.sh

---
+++ build.sh
@@ -0,0 +1,10 @@
+# https://wails.io/docs/reference/cli#platforms
+
+wails build -platform darwin/universal 
+
+wails build -platform windows/amd64
+wails build -platform windows/arm64
+
+wails build -platform linux/amd64
+wails build -platform linux/arm64
+
